e7227ec7314ad14fd104380b4627d31f4b49099c
Nikita Yankovskii <yannik98@yandex.ru>
2020-11-21 21:08:52 +0300

    added JSONreader_beta

---
+++ JSONReader.cpp
@@ -0,0 +1,5 @@
+//
+// Created by justnik on 21.11.2020.
+//
+
+#include "JSONReader.h"
---
+++ JSONReader.h
@@ -0,0 +1,14 @@
+//
+// Created by justnik on 21.11.2020.
+//
+
+#ifndef O_RAN_JSONREADER_H
+#define O_RAN_JSONREADER_H
+
+
+class JSONReader {
+
+};
+
+
+#endif //O_RAN_JSONREADER_H
